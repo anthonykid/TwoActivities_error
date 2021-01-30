e6a2c1bc1a521f7ba166f48182768ecac8ae2af6
anthonykid <dickya630@gmail.com>
2021-01-30 20:08:16 +0800

    newest-anthony

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import static android.util.Log.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+}
